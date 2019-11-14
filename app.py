b848767f4920b1a13d06d0969169e1510c32f726
Cristh <camilo.r13@hotmail.com>
2019-11-14 09:49:05 -0500

    Coneccion con mysql

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'lacatuli'
+mysql = MySQL(app)
 
 @app.route('/')
+def login():
+	return render_template("login.html")
+
+@app.route('/index')
 def index():
 	return render_template("index.html")
 
